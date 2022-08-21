3d8eae7646f6d868290df32e9fb6c7c632a83941
Roman <igoshinr.estho@gmail.com>
2022-08-21 06:18:11 +0300

    56

---
+++ 56/Program.cs
@@ -0,0 +1,30 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n]; 
+int[] arr = new int[m];
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+        arr[i] += matrix[i, j];
+    }
+}
+
+int min = arr[0];
+int ind = 0;
+
+for (int i = 0; i < m; i++)
+{
+    if (arr[i] < min)
+    {
+        min = arr[i];
+        ind = i;
+    }
+}
+
+Console.WriteLine();
+Console.WriteLine(ind);
