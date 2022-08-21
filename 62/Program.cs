8d24b4a0a29236597c5579e7f8153a340929a862
Roman <igoshinr.estho@gmail.com>
2022-08-21 06:19:01 +0300

    62

---
+++ 62/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.Write("Введите размер массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[n, n];
+int x = 0;
+int y = 0;
+int size = n * n;
+
+for (int i = 1; i <= size; i++)
+{
+    matrix[x, y] = i;
+    if (x == 0 || y == 0 || x == n - 1 || y == n - 1)
+    {
+        if (y + 1 != n && x == 0) y++;
+        else if (x + 1 != n && y == n -1) x++;
+        else if (y != 0 && x == n - 1) y--;
+        else if (matrix[x - 1, y] == 0) x--;
+        else y++;
+    }
+    else
+    {
+        if (matrix[x, y + 1] == 0) y++;
+        else if (matrix[x + 1, y] == 0) x++;
+        else if (matrix[x, y - 1] == 0) y--;
+        else x--;
+    } 
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    Console.Write(matrix[i, j] + "\t");
+}
