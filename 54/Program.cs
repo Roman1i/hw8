ff5e2e330ecd18aeb49c296918c4d941236d816e
Roman <igoshinr.estho@gmail.com>
2022-08-21 06:15:24 +0300

    54

---
+++ 54/Program.cs
@@ -0,0 +1,39 @@
+﻿int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] matrix = new int[m, n];
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = new Random().Next(1, 10);
+        Console.Write(matrix[i, j] + " ");
+    }
+}
+
+int tmp;
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n - 1; j++)
+    {
+        if (matrix[i, j] > matrix[i, j + 1])
+        {
+            tmp = matrix[i, j];
+            matrix[i, j] = matrix[i, j + 1];
+            matrix[i, j + 1] = tmp;
+            j = - 1;
+        }
+    }
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < m; i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write(matrix[i, j] + " ");
+    }
+}
