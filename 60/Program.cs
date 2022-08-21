e6e3471b2a6a139742dfc29082ed0d6316e4b814
Roman <igoshinr.estho@gmail.com>
2022-08-21 06:18:45 +0300

    60

---
+++ 60/Program.cs
@@ -0,0 +1,19 @@
+﻿int x = Convert.ToInt32(Console.ReadLine());
+int y = Convert.ToInt32(Console.ReadLine());
+int z = Convert.ToInt32(Console.ReadLine());
+int[,,] cube = new int[x, y, z];
+
+for (int i = 0; i < cube.GetLength(0); i++)
+{
+    Console.WriteLine();
+    for (int j = 0; j < cube.GetLength(1); j++)
+    {
+        Console.WriteLine();
+        for (int k = 0; k < cube.GetLength(2); k++)
+        {
+            cube[i, j, k] = new Random().Next(10, 100);
+            Console.Write(cube[i, j, k] + " ");
+
+        }
+    }
+}
